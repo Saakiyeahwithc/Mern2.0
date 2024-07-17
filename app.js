4fe5a3ad3ad4aca6857a62c06b49228961aedbcd
saakiyeahwith.c <prashant1.785401@cct.tu.edu.np>
2024-07-17 09:45:57 +0545

    Day-2 commit

--- app.js
+++ app.js
@@ -4,3 +4,17 @@ const app = express()
 //alternative
 //const app = require ('express')
 
+app.get("/", (req, res) => {
+    res.send("See you World!")
+})
+
+
+
+
+
+
+
+
+app.listen(4000, () => {
+    console.log('Nodejs server has started at port 3000');
+}) 
